64a9aa2ede129eabb617a33448b8a65c61ddc502
elizabethjump <ejump@smcgov.org>
2024-03-26 13:17:49 -0700

    updating notes

--- retrieve-username-current-session.R
+++ retrieve-username-current-session.R
@@ -1,4 +1,4 @@
-# code to get environment variables to be used in the session
+# purpose: get the username of the person in the current session
+# notes: this is useful when dynamically creating usernames in a script, ex: paste0(Sys.getenv("USERNAME"), "@domain.gov") would create email addresses for a given organization
 
-# get the username --------------------------------------
 Sys.getenv("USERNAME")
