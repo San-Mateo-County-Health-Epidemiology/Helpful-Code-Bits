b89fe55be175f2bdef923ae33d042b7c463766c2
elizabethjump <ejump@smcgov.org>
2024-03-26 13:15:28 -0700

    adding script with packages to install

---
+++ packages-to-install-with-new-R-installation.R
@@ -0,0 +1,4 @@
+# purpose: easiyl re-install the packages you need when you download a new version of R
+# notes: please add any packages you commonly use to this list so it's helpful and up to date. 
+
+install.packages(c("tidyverse", "readxl", "writexl", "rio", "zoo", "dbplyr", "odbc", "DBI", "lubridate", "httr", "config", "fastLink", "RecordLinkage", "flextable", "jsonlite", "redcapAPI", "sendmailR", "fs", "boxr", "jose", "RCurl", "acs", "tidycensus", "sf", "tigris", "usethis", "shiny", "shinydashboard", "plotly", "shinythemes", "DT", "janitor", "arrow", "devtools"))
