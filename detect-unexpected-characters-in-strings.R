053aece4cbbb4b015496468aa5803fadb1c372cf
elizabethjump <ejump@smcgov.org>
2024-07-03 15:58:33 -0700

    adding encoding(var) <- "latin1"

--- detect-unexpected-characters-in-strings.R
+++ detect-unexpected-characters-in-strings.R
@@ -4,3 +4,6 @@
 data %>%
   mutate(spec_char = str_detect(val, regex("[^A-Za-z0-9\\s\\.\\,\\%\\?\\/\\-\\'\\(\\)\\&\\;\\:\\#\\*\\_\\\\\\#\\•\\~Ñó]"))) %>%
   filter(spec_char == T)
+
+# if you need to recode everything quickly, you can do this
+Encoding(data$val) <- "latin1"
