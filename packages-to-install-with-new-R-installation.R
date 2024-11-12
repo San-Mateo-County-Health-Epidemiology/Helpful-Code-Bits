cbf0e1674d489bbda98c1f5a03c76cdf9e18babf
elizabethjump <ejump@smcgov.org>
2024-11-12 15:54:04 -0800

    adding smcepi to the list of packages

--- packages-to-install-with-new-R-installation.R
+++ packages-to-install-with-new-R-installation.R
@@ -2,3 +2,6 @@
 # notes: please add any packages you commonly use to this list so it's helpful and up to date. 
 
 install.packages(c("tidyverse", "readxl", "writexl", "rio", "zoo", "dbplyr", "odbc", "DBI", "lubridate", "httr", "config", "fastLink", "RecordLinkage", "flextable", "jsonlite", "redcapAPI", "sendmailR", "fs", "boxr", "jose", "RCurl", "acs", "tidycensus", "sf", "tigris", "usethis", "shiny", "shinydashboard", "plotly", "shinythemes", "DT", "janitor", "arrow", "devtools"))
+
+# install.packages("devtools")
+devtools::install_github("San-Mateo-County-Health-Epidemiology/smcepi")
