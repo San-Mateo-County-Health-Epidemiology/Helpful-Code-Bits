4d2c927b304a8f530535cd64dc6067285a097950
Elizabeth Jump <42222436+elizabethjump@users.noreply.github.com>
2025-06-13 09:34:00 -0700

    Update packages-to-install-with-new-R-installation.R
    adding here and see

--- R/packages-to-install-with-new-R-installation.R
+++ R/packages-to-install-with-new-R-installation.R
@@ -1,7 +1,7 @@
 # purpose: easily re-install the packages you need when you download a new version of R
 # notes: please add any packages you commonly use to this list so it's helpful and up to date. 
 
-install.packages(c("tidyverse", "readxl", "writexl", "rio", "zoo", "dbplyr", "odbc", "DBI", "lubridate", "httr", "config", "fastLink", "RecordLinkage", "flextable", "jsonlite", "redcapAPI", "sendmailR", "fs", "boxr", "jose", "RCurl", "acs", "tidycensus", "sf", "tigris", "usethis", "shiny", "shinydashboard", "plotly", "shinythemes", "DT", "janitor", "arrow", "devtools"))
+install.packages(c("tidyverse", "readxl", "writexl", "rio", "zoo", "dbplyr", "odbc", "DBI", "lubridate", "httr", "config", "fastLink", "RecordLinkage", "flextable", "jsonlite", "redcapAPI", "sendmailR", "fs", "boxr", "jose", "RCurl", "acs", "tidycensus", "sf", "tigris", "usethis", "shiny", "shinydashboard", "plotly", "shinythemes", "DT", "janitor", "arrow", "devtools", "here", "see"))
 
 # install.packages("devtools")
 devtools::install_github("San-Mateo-County-Health-Epidemiology/smcepi")
