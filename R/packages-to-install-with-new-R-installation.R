2fe7cadfbfad9c396c7660d2b6937b4dadae271a
Beth Jump <bjump2@gmail.com>
2026-07-15 16:49:57 -0700

    updating list of packages to install with a new R version

--- R/packages-to-install-with-new-R-installation.R
+++ R/packages-to-install-with-new-R-installation.R
@@ -1,7 +1,8 @@
 # purpose: easily re-install the packages you need when you download a new version of R
 # notes: please add any packages you commonly use to this list so it's helpful and up to date. 
 
-install.packages(c("tidyverse", "readxl", "writexl", "rio", "zoo", "dbplyr", "odbc", "DBI", "lubridate", "httr", "config", "fastLink", "RecordLinkage", "flextable", "jsonlite", "redcapAPI", "sendmailR", "fs", "boxr", "jose", "RCurl", "acs", "tidycensus", "sf", "tigris", "usethis", "shiny", "shinydashboard", "plotly", "shinythemes", "DT", "janitor", "arrow", "devtools", "here", "see", "blastula", "keyring"))
+install.packages(c("acs", "airnow", "arrow", "blastula", "boxr", "config", "DBI", "dbplyr", "devtools", "DT", "fastLink", "flextable", "fs", "here", "httr", "janitor", "jose", "jsonlite", "kableExtra", "keyring", "lubridate", "MMWRweek", "odbc", "pdftools", "plotly", "RCurl", "readr", "readxl", "RecordLinkage", "redcapAPI", "rio", "runner", "see", "sendmailR", "sf", "shiny", "shinydashboard", "shinythemes", "tidycensus", "tidyverse", "tigris", "usethis", "writexl", "zoo"))
 
 # install.packages("devtools")
 devtools::install_github("San-Mateo-County-Health-Epidemiology/smcepi")
+devtools::install_github("cdcgov/Rnssp")
