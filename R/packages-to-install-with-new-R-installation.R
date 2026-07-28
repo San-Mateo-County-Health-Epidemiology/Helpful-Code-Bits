279a6dd46060f80e90dc0b35e761cbf5e817a5eb
Beth Jump <bjump2@gmail.com>
2026-07-28 08:31:39 -0700

    adding gt to packages-to-install-with-new-R-installation

--- R/packages-to-install-with-new-R-installation.R
+++ R/packages-to-install-with-new-R-installation.R
@@ -1,7 +1,7 @@
 # purpose: easily re-install the packages you need when you download a new version of R
 # notes: please add any packages you commonly use to this list so it's helpful and up to date. 
 
-install.packages(c("acs", "airnow", "arrow", "blastula", "boxr", "config", "DBI", "dbplyr", "devtools", "DT", "fastLink", "flextable", "fs", "here", "httr", "janitor", "jose", "jsonlite", "kableExtra", "keyring", "lubridate", "MMWRweek", "odbc", "pdftools", "plotly", "RCurl", "readr", "readxl", "RecordLinkage", "redcapAPI", "rio", "runner", "see", "sendmailR", "sf", "shiny", "shinydashboard", "shinythemes", "tidycensus", "tidyverse", "tigris", "usethis", "writexl", "zoo"))
+install.packages(c("acs", "airnow", "arrow", "blastula", "boxr", "config", "DBI", "dbplyr", "devtools", "DT", "fastLink", "flextable", "fs", "gt", "here", "httr", "janitor", "jose", "jsonlite", "kableExtra", "keyring", "lubridate", "MMWRweek", "odbc", "pdftools", "plotly", "RCurl", "readr", "readxl", "RecordLinkage", "redcapAPI", "rio", "runner", "see", "sendmailR", "sf", "shiny", "shinydashboard", "shinythemes", "tidycensus", "tidyverse", "tigris", "usethis", "writexl", "zoo"))
 
 # install.packages("devtools")
 devtools::install_github("San-Mateo-County-Health-Epidemiology/smcepi")
